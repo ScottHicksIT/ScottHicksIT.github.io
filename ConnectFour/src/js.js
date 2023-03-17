1141122941f410e4a9fe6f3265a40c9efca7cc93
ScottHicksIT <scott.hicks.c@gmail.com>
2023-03-17 23:52:58 +1100

    Update - Tokens that have already been selected can now not be selected again.

--- ConnectFour/src/js.js
+++ ConnectFour/src/js.js
@@ -8,6 +8,7 @@ function coordinate(x, y) {
 
 var playerOneCoords = new Array();
 var playerTwoCoords = new Array();
+var activeTokens = new Array();
 
 playerOneCoords.push(new coordinate(10, 0));
 playerOneCoords.push(new coordinate(1, 0));
@@ -16,32 +17,54 @@ playerOneCoords.push(new coordinate(12, 7));
 console.log(playerOneCoords);
 
 
+function updateToken(clicked_id){
+    //Check players turn
+    if (playerOneTurn == true){
+
+    } else {
+        
+    }
+    changeStatus(clicked_id); 
+}
+
+
+
 
 function changeStatus(clicked_id){
+    //Recieves the text coordinate and creates it as array with two elements
     let text = clicked_id;
     let coordArrary = text.split(",")
     let x = coordArrary[0]
     let y = coordArrary[1]
-    if (playerOneTurn == true){
-        document.getElementById(clicked_id).style.backgroundColor = "red";
-        playerOneCoords.push(new coordinate(parseInt(x), parseInt(y)));
-        playerOneTurn = false;
-    } else {
-        document.getElementById(clicked_id).style.backgroundColor = "yellow";
-        playerTwoCoords.push(new coordinate(parseInt(x), parseInt(y)));
-        playerOneTurn = true;
+
+    if (checkIfPlayAbleToken(x,y) == true) {
+        if (playerOneTurn == true){
+            document.getElementById(clicked_id).style.backgroundColor = "red";
+            playerOneCoords.push(new coordinate(parseInt(x), parseInt(y)));
+            activeTokens.push(new coordinate(parseInt(x), parseInt(y)));
+            document.getElementById("playerTurn").innerHTML = "Player Two's Turn";
+            playerOneTurn = false;
+        } else {
+            document.getElementById(clicked_id).style.backgroundColor = "yellow";
+            playerTwoCoords.push(new coordinate(parseInt(x), parseInt(y)));
+            activeTokens.push(new coordinate(parseInt(x), parseInt(y)));
+            document.getElementById("playerTurn").innerHTML = "PLayer One's Turn";
+            playerOneTurn = true;
+        }
+    } 
+}
+
+function checkIfPlayAbleToken(x,y) {
+    let playable = true;
+    for (let i = 0; i < activeTokens.length; i++) {
+        if (x == activeTokens[i].x && y == activeTokens[i].y) {
+            // alert("This token has alread been selected");
+            playable = false;
+        } 
     }
+    return playable;
 }
 
 
-function updateToken(clicked_id){
-    //Check players turn
-    if (playerOneTurn == true){
-        document.getElementById("playerTurn").innerHTML = "Player Two's Turn";
-    } else {
-        document.getElementById("playerTurn").innerHTML = "PLayer One's Turn";
-    }
-    changeStatus(clicked_id);
-    
-}
+
 
