c5a2895c71e7e3bd7378c92c47ed7568efe4fd65
ScottHicksIT <scott.hicks.c@gmail.com>
2023-06-25 18:41:53 +1000

    Update - Diagonal connections now detected

--- ConnectFour/src/js.js
+++ ConnectFour/src/js.js
@@ -3,7 +3,7 @@ let playerOneTurn = true;
 let gameOver = false;
 var playerOneCoords = new Array();
 var playerTwoCoords = new Array();
-var activeTokens = new Array();
+
 
 //Token Object
 class Token {
@@ -12,7 +12,6 @@ class Token {
         this.y = y;
         this.owner = owner;
     }
-
     getCoordinates() {
         return this.x + "," + this.y;
     }
@@ -24,7 +23,93 @@ class Token {
     }
 }
 
+class Board {
+    constructor(h, w){
+        this.height = h; 
+        this.width = w; 
+    }
+    getHeight(){
+        return this.height;
+    }
+    getWidth(){
+        return this.width;
+    }
+}
 
+class Position {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.empty = true;
+        this.player = null;
+    }
+    getCoordinates() {
+        return this.x + "," + this.y;
+    }
+    getYCoord() {
+        return this.y;
+    }
+    getXCoord() {
+        return this.x;
+    }
+
+    setPlayer(player){
+        this.player = player;
+    }
+    getPlayer(){
+        return this.player;
+    }
+    getPosDiagonal(){
+        let x1 = this.x + 1;
+        let y1 = this.y + 1; 
+        let diagonal = x1 + "," + y1;
+        return diagonal;
+    }
+    getNegDiagonal(){
+        let x1 = this.x - 1;
+        let y1 = this.y + 1; 
+        let diagonal = x1 + "," + y1;
+        return diagonal;
+    }
+}
+
+function play(){
+    
+}
+
+
+//Used to get index number based of x and y 
+function getIndexNumber(x,y){
+    for (let i = 0; i < positions.length;i++){
+        if (positions[i].getXCoord() == x && positions[i].getYCoord() == y){
+            return i;
+        }
+    }
+}
+
+//Used to get index number based of coordinates
+function getIndexNumberCoord(a){
+    let coordArrary = a.split(",");
+    let x = coordArrary[0];
+    let y = coordArrary[1];
+    for (let i = 0; i < positions.length;i++){
+        if (positions[i].getXCoord() == x && positions[i].getYCoord() == y){
+            return i;
+        }
+    }
+}
+
+//Create Board
+b = new Board(6,7);
+let positions = new Array;
+//Create Positions 
+for (let i=1;i <= b.getHeight();i++){
+    for (let j=1;j <= b.getWidth();j++){
+        a = new Position(j,i);
+        //alert(i + "," + j);
+        positions.push(a);
+    } 
+}
 
 //This runs if a player selects a token. 
 function updateToken(clicked_id){
@@ -38,39 +123,50 @@ function updateToken(clicked_id){
         if (playerOneTurn == true){
             document.getElementById(clicked_id).style.backgroundColor = "red";
             playerOneCoords.push(new Token(parseInt(x), parseInt(y), "PLayerOne"));
-            activeTokens.push(new Token(parseInt(x), parseInt(y), "PLayerOne"));
-            document.getElementById("playerTurn").innerHTML = "Player Two's Turn";   
+            document.getElementById("playerTurn").innerHTML = "Player Two's Turn"; 
+            
+            //Position Object
+            for (let i = 0; i < positions.length;i++){
+                if (positions[i].getXCoord() == x && positions[i].getYCoord() == y){
+                    positions[i].setPlayer("Player 1");
+                }
+            }
+
             playerOneTurn = false;
         } else {
             document.getElementById(clicked_id).style.backgroundColor = "yellow";
             playerTwoCoords.push(new Token(parseInt(x), parseInt(y), "PLayerTwo"));
-            activeTokens.push(new Token(parseInt(x), parseInt(y), "PLayerTwo"));
             document.getElementById("playerTurn").innerHTML = "Player One's Turn";
             playerOneTurn = true;
+
+            //Position Object
+            for (let i = 0; i < positions.length;i++){
+                if (positions[i].getXCoord() == x && positions[i].getYCoord() == y){
+                    positions[i].setPlayer("Player 2");
+                }
+            }
         }
     } 
-    checkWinner();
+    checkWinner(x,y);
 }
 
 //Once token has been selected, this checks if the token has already been played or if there is a token below the selected token. 
 function checkIfPlayAbleToken(x,y) {
     //Check if token has already been played
     let playable = true;
-    for (let i = 0; i < activeTokens.length; i++) {
-        //Check if token is currently already active. 
-        if (x == activeTokens[i].x && y == activeTokens[i].y) {
-            // alert("This token has alread been selected");
-            playable = false;
-        } 
+    
+    if (positions[(getIndexNumber(x,y))].getPlayer() != null){
+        //alert("Position Invalid");
+        playable = false;
     }
 
     //Check to see if the token is placed above another token
     //If token is placed on bottom row, it does not check. 
+
     if (y != 1){
-        let tokenBelow = false;
-        for (let i = 0; i < activeTokens.length; i++) {
-            if (x == activeTokens[i].x && y-1 == (activeTokens[i].y)){
-                
+        let tokenBelow = false; 
+        for (let i = 0; i < positions.length; i++){
+            if ((x == positions[i].x && y-1 == (positions[i].y) && positions[i].getPlayer() != null )){
                 tokenBelow = true;
             }
         }
@@ -78,17 +174,18 @@ function checkIfPlayAbleToken(x,y) {
             playable = false;
           
         }
-    } 
+
+    }
     return playable;
 }
 
 
 //This is ran before the player has clicked a token. 
 //If the token does not have a token below it, the token box shadow turns red. 
-function checkToken(hover_id){
+function checkToken(id){
     //Check to see if the token is placed above another token
     //If token is placed on bottom row, it does not check.
-    let text = hover_id;
+    let text = id;
     let coordArrary = text.split(",")
     let x = coordArrary[0]
     let y = coordArrary[1]
@@ -96,10 +193,11 @@ function checkToken(hover_id){
     var elem = document.getElementById(tokenID); 
 
     //Checks if a token is below
+
     if (y != 1){
-        let tokenBelow = false;
-        for (let i = 0; i < activeTokens.length; i++) {
-            if (x == activeTokens[i].x && y-1 == (activeTokens[i].y)){
+        let tokenBelow = false; 
+        for (let i = 0; i < positions.length; i++){
+            if ((x == positions[i].x && y-1 == (positions[i].y) && positions[i].getPlayer() != null )){
                 tokenBelow = true;
             }
         }
@@ -107,12 +205,13 @@ function checkToken(hover_id){
             playable = false;
             elem.style.boxShadow = "0px 0px 30px 5px red";
         }
-    } 
+
+    }
 }
 
 //Resets the token's box shadow once the player moves away from the token they are mousing over. 
-function resetToken(hover_id){
-    let text = hover_id;
+function resetToken(id){
+    let text = id;
     let coordArrary = text.split(",")
     let x = coordArrary[0]
     let y = coordArrary[1]
@@ -121,109 +220,193 @@ function resetToken(hover_id){
     elem.style.boxShadow = "0px 0px 0px 0px red";
 }
 
-
-
-
 //Check who has won
-function checkWinner(){   
+function checkWinner(x,y){   
     let count = 0;
     let tokenObjectPos = new Array();
+    let positionIDs = new Array();
+    let positionObjs = new Array();
     
-    //Check Vertical
+    //Check Vertical Player 1
     //Number of of Columns (0 to 7)
     for (let i = 1; i < 7; i++) {
-        
         count = 0;
-        tokenObjectPos = [];
-        for (let j = 0; j < playerOneCoords.length; j++){ 
-            if (playerOneCoords[j].x == i){
+        positionIDs = [];
+        for (let j = 0; j < positions.length; j++){ 
+            if (positions[j].x == i && positions[j].getPlayer() == "Player 1"){
                 let currentIndex = j;
-                tokenObjectPos.push(currentIndex);
+                positionIDs.push(currentIndex);
                 count++;
             }
         }
         if (count >= 4){
-            checkIfVertical(tokenObjectPos, "Player1");
+            checkIfVertical(positionIDs, "Player 1");
         }
         
-        //Check Vertical
+        //Check Vertical Player 2
         count = 0;
-        tokenObjectPos = [];
-
-        for (let j = 0; j < playerTwoCoords.length; j++){ 
-            if (playerTwoCoords[j].x == i){
+        positionIDs = [];
+        for (let j = 0; j < positions.length; j++){ 
+            if (positions[j].x == i && positions[j].getPlayer() == "Player 2"){
                 let currentIndex = j;
-                tokenObjectPos.push(currentIndex);
+                positionIDs.push(currentIndex);
                 count++;
             }
         }
         if (count >= 4){
-            checkIfVertical(tokenObjectPos, "Player2");
-        }  
+            checkIfVertical(positionIDs, "Player 2");
+        }
     }  
 
-    //Check Horizontal Connects
+    //Check Horizontal Connects for Player 1
     for (let i=1; i < 6; i++){
         count = 0;
-        tokenObjectPos = [];
-        for (let j = 0; j < playerOneCoords.length; j++){ 
-            if (playerOneCoords[j].y == i){
-                let currentIndex = playerOneCoords[j];
-                tokenObjectPos.push(currentIndex);
+        positionIDs = [];
+        positionObjs = [];
+        for (let j = 0; j < positions.length; j++){ 
+            if (positions[j].y == i && positions[j].getPlayer() == "Player 1"){
+                positionIDs.push(j);
+                positionObjs.push(positions[j]);
                 count++;
             }
         } 
     
         if (count >= 4) {
-             for (let n = 0; n < tokenObjectPos.length; n++){
-                console.log(tokenObjectPos[n].getCoordinates());
-             }
-             checkIfHorizontal(tokenObjectPos, "Player1");
-           
+            checkIfHorizontal(positionIDs, "Player 1", positionObjs);
+        }
+    }
+    //Check Horizontal Connects for Player 2
+    for (let i=1; i < 6; i++){
+        count = 0;
+        positionIDs = [];
+        positionObjs = [];
+        for (let j = 0; j < positions.length; j++){ 
+            if (positions[j].y == i && positions[j].getPlayer() == "Player 2"){
+                positionIDs.push(j);
+                positionObjs.push(positions[j]);
+                count++;
+            }
+        } 
+    
+        if (count >= 4) {
+            checkIfHorizontal(positionIDs, "Player 2", positionObjs);
         }
     }
 
-    for (let i=1; i < 6; i++){
-        count = 0;
-        tokenObjectPos = [];
-        for (let j = 0; j < playerTwoCoords.length; j++){ 
-            if (playerTwoCoords[j].y == i){
-                let currentIndex = playerTwoCoords[j];
-                tokenObjectPos.push(currentIndex);
-                count++;
-            }
-        } 
+    //Check Positive Diagonal Connection
+   
+    for (let i = 0; i < 42; i++){
+        let player = null;
+        let tokenIndex = null; 
+        let diagonalTokenIndex = null;
+        let diagonalPlayer = null;
+
+        let tokenIndexs = new Array();
+        tokenIndex = i;
+
+        tokenIndexs.push(i);
+        player = positions[i].getPlayer();
+        diagonalTokenIndex = getIndexNumberCoord(positions[i].getPosDiagonal());
+        try {
+            diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+        } catch(error) {
+            console.log(".")
+        }
+        
     
-        if (count >= 4) {
-             for (let n = 0; n < tokenObjectPos.length; n++){
-                console.log(tokenObjectPos[n].getCoordinates());
-             }
-             checkIfHorizontal(tokenObjectPos, "Player2");
+        //Check First Diagonal Connection 
+        if (player!= null && player == diagonalPlayer){
+          tokenIndex = diagonalTokenIndex;
+          player = positions[diagonalTokenIndex].getPlayer();
+          diagonalTokenIndex = getIndexNumberCoord(positions[tokenIndex].getPosDiagonal());
+          diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+          tokenIndexs.push(tokenIndex);
+          
+             
+          //Check Second Diagonal Connection 
+          if (player!= null && player == diagonalPlayer){
+            tokenIndex = diagonalTokenIndex;
+            player = positions[diagonalTokenIndex].getPlayer();
+            diagonalTokenIndex = getIndexNumberCoord(positions[tokenIndex].getPosDiagonal());
+            diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+            tokenIndexs.push(diagonalTokenIndex);
            
+
+            //Check Third Diagonal Connection
+            if (player!= null && player == diagonalPlayer){
+                tokenIndexs.push(tokenIndex);
+                gameOverPhase(player,tokenIndexs);
+            }
+          }
         }
     }
+
+    //Check Negative Diagonal Connection
+   
+    for (let i = 0; i < 42; i++){
+        let player = null;
+        let tokenIndex = null; 
+        let diagonalTokenIndex = null;
+        let diagonalPlayer = null;
+
+        let tokenIndexs = new Array();
+        tokenIndex = i;
+
+        tokenIndexs.push(i);
+        player = positions[i].getPlayer();
+        diagonalTokenIndex = getIndexNumberCoord(positions[i].getNegDiagonal());
+        try {
+            diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+        } catch(error) {
+            console.error(error);
+        }
+        
+        //Check First Diagonal Connection 
+        if (player!= null && player == diagonalPlayer){
+          tokenIndex = diagonalTokenIndex;
+          player = positions[diagonalTokenIndex].getPlayer();
+          diagonalTokenIndex = getIndexNumberCoord(positions[tokenIndex].getNegDiagonal());
+          diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+          tokenIndexs.push(tokenIndex);
+          
+          //Check Second Diagonal Connection 
+          if (player!= null && player == diagonalPlayer){
+            tokenIndex = diagonalTokenIndex;
+            player = positions[diagonalTokenIndex].getPlayer();
+  
+            diagonalTokenIndex = getIndexNumberCoord(positions[tokenIndex].getNegDiagonal());
+            diagonalPlayer = positions[diagonalTokenIndex].getPlayer();
+            tokenIndexs.push(diagonalTokenIndex);
+            
+            //Check Third Diagonal Connection
+            if (player!= null && player == diagonalPlayer){
+                tokenIndexs.push(tokenIndex);
+                gameOverPhase(player,tokenIndexs);
+            }
+          }
+        }
+    }
+
 }   
 
-function checkIfVertical(tokenIndexs, player){
+function checkIfVertical(positionIDs, player){
     let sequentialOrder = true;
     let firstToken;
     let secondToken;
     let thirdToken;
     let fourthToken;
- 
-    for (let i = 0; i < tokenIndexs.length; i++){
-        if (player == "Player1"){
-            alert("Player1");
-            firstToken = playerOneCoords[tokenIndexs[i]];
-            secondToken = playerOneCoords[tokenIndexs[i+1]];
-            thirdToken = playerOneCoords[tokenIndexs[i+2]];
-            fourthToken = playerOneCoords[tokenIndexs[i+3]];
-        } else if (player == "Player2") {
-            alert("Player2");
-            firstToken = playerTwoCoords[tokenIndexs[i]];
-            secondToken = playerTwoCoords[tokenIndexs[i+1]];
-            thirdToken = playerTwoCoords[tokenIndexs[i+2]];
-            fourthToken = playerTwoCoords[tokenIndexs[i+3]];
+    
+    for (let i = 0; i < positionIDs.length; i++){
+        if (player == "Player 1"){
+            firstToken = positions[positionIDs[i]];
+            secondToken = positions[positionIDs[i+1]];
+            thirdToken = positions[positionIDs[i+2]];
+            fourthToken = positions[positionIDs[i+3]];
+        } else if (player == "Player 2") {
+            firstToken = positions[positionIDs[i]];
+            secondToken = positions[positionIDs[i+1]];
+            thirdToken = positions[positionIDs[i+2]];
+            fourthToken = positions[positionIDs[i+3]];
         }
 
         //Check if tokens are in order vertically
@@ -244,24 +427,13 @@ function checkIfVertical(tokenIndexs, player){
         }
 
         if(sequentialOrder == true){
-            alert("The tokens are in order!!!");
-            let tokenID;
-            for (k = i; k < tokenIndexs.length; k++){
-            if (player == "Player1"){
-                tokenID = playerOneCoords[tokenIndexs[k]].getCoordinates();
-            } else if (player == "Player2") {
-                tokenID = playerTwoCoords[tokenIndexs[k]].getCoordinates();
-            }
-            var elem = document.getElementById(tokenID); 
-            elem.style.boxShadow = "0px 0px 30px 5px white";
-            }  
-            alert("Game is over")
+            gameOverPhase(player,positionIDs)
             break;
         }
     }
 }
 
-function checkIfHorizontal(tokenIndexs, player) {
+function checkIfHorizontal(positionIDs, player, positionObjs) {
     let sequentialOrder = true;
 
     let firstToken;
@@ -269,13 +441,13 @@ function checkIfHorizontal(tokenIndexs, player) {
     let thirdToken;
     let fourthToken;
 
-    for (let i = 0; i < tokenIndexs.length; i++){
+    for (let i = 0; i < positionObjs.length; i++){
         //Order Tokens by X Value
-        tokenIndexs.sort(function(a, b){return a.x - b.x});
-        firstToken = tokenIndexs[i];
-        secondToken = tokenIndexs[i+1];
-        thirdToken = tokenIndexs[i+2];
-        fourthToken = tokenIndexs[i+3];
+        positionObjs.sort(function(a, b){return a.x - b.x});
+        firstToken = positionObjs[i];
+        secondToken = positionObjs[i+1];
+        thirdToken = positionObjs[i+2];
+        fourthToken = positionObjs[i+3];
 
         let selectedTokens = new Array;
         selectedTokens = [];
@@ -302,20 +474,17 @@ function checkIfHorizontal(tokenIndexs, player) {
         }
 
         if(sequentialOrder == true){
-            alert("The tokens are in order!!!");
-            let tokenID;
-            for (k = 0; k < selectedTokens.length; k++){
-                tokenID = selectedTokens[k].getCoordinates();
-                var elem = document.getElementById(tokenID); 
-                elem.style.boxShadow = "0px 0px 30px 5px white";
-            }  
-
-            if (player == "Player 1"){
-                alert("Game is over. Player 1 wins!");
-            } else {
-                alert("Game is over. Player 2 wins!");
-            }
-            
+            gameOverPhase(player,positionIDs);
         }
+    }
+}
+
+function gameOverPhase(winner, selectedTokens){
+    alert(winner + " Wins!");
+    for (k = 0; k <= 4; k++){
+        let tokenID = positions[selectedTokens[k]];
+        tokenID = tokenID.getCoordinates();
+        let elem = document.getElementById(tokenID); 
+        elem.style.boxShadow = "0px 0px 30px 5px white";
     }
 }
